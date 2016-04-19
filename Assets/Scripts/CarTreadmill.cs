9d71b196aba47624ccfdca37b9a2b97c86c24b10
Kyle Lin <kylelin47@gmail.com>
2016-04-19 10:25:44 -0400

    Add dealership, cars driving by

---
+++ Assets/Scripts/CarTreadmill.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarTreadmill : MonoBehaviour {
+	public float speed;
+	private Vector3 initialPosition;
+	// Use this for initialization
+	void Start () {
+		initialPosition = transform.position;
+	}
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.left * Time.deltaTime * speed);
+	}
+	void OnTriggerEnter(Collider other) {
+		transform.position = initialPosition;
+		speed += Random.Range (-2, 2);
+	}
+}
---
+++ Assets/Scripts/ChoiceController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChoiceController : MonoBehaviour {
+	public bool choice1;
+	public bool choice2;
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (transform.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
