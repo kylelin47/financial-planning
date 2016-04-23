46584de2c30f34b002e8526c3266aabc3b18b1da
Kyle Lin <kylelin47@gmail.com>
2016-04-23 12:08:24 -0400

    Parents player to car

--- Assets/Scripts/SwitchCars.cs
+++ Assets/Scripts/SwitchCars.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class SwitchCars : MonoBehaviour {
 	public GameObject player;
+	public Transform civicTransform;
 	public Transform civicWaypoint;
 	private ChoiceController cc;
 	// Use this for initialization
@@ -10,6 +11,7 @@ public class SwitchCars : MonoBehaviour {
 		cc = (ChoiceController) FindObjectOfType(typeof(ChoiceController));
 		if (!cc.choice1) {
 			player.transform.position = civicWaypoint.position;
+			player.transform.parent = civicTransform;
 		}
 	}
 	// Update is called once per frame
