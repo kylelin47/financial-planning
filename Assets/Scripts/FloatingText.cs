6bf8cb538cff4a7dd264a2b3e7a66914de74dd51
Kyle Lin <kylelin47@gmail.com>
2016-04-25 12:57:47 -0400

    Add intro sequence

--- Assets/Scripts/FloatingText.cs
+++ Assets/Scripts/FloatingText.cs
@@ -2,10 +2,22 @@
 using System.Collections;
 public class FloatingText : MonoBehaviour {
 	public Transform target;
+	public float timeDisabled;
 	public float timeActive;
+	private Renderer[] renderers;
 	// Use this for initialization
 	void Start () {
-		Destroy (gameObject, timeActive);
+		renderers = GetComponentsInChildren<Renderer> ();
+		foreach (Renderer r in renderers) {
+			r.enabled = false;
+		}
+		Invoke ("ShowText", timeDisabled);
+		Destroy (gameObject, timeDisabled + timeActive);
+	}
+	private void ShowText() {
+		foreach (Renderer r in renderers) {
+			r.enabled = true;
+		}
 	}
 	// Update is called once per frame
 	void Update () {
