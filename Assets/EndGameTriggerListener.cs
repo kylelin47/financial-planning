508753d463c22d8f75b0120d53734b177967b5b9
Zane Hooper <zanehoop@gmail.com>
2016-04-28 03:32:43 -0400

    End game trigger

---
+++ Assets/EndGameTriggerListener.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGameTriggerListener : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Cardboard.SDK.CardboardTriggered) {
+			Application.LoadLevel(0);
+		}
+	}
+}
